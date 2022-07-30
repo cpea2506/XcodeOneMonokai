d666367b4e06f3d05197e0c89e856aed2d03ee64
CPea <vienpro2506@gmail.com>
2022-07-30 22:33:27 +0700

    update theme

--- OneMonokai/install.sh
+++ OneMonokai/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #  install.sh
 #  OneMonokai
@@ -6,59 +6,58 @@
 #  Created by Đỗ Viên on 18/01/2022.
 #
 
-declare -r XCODE_DIR=$HOME/Library/Developer/Xcode
-declare -r THEME_DIR=$XCODE_DIR/UserData/FontAndColorThemes
-declare -r XCODE_ONEMONOKAI_DIR=$PWD/Xcode-OneMonokai
+declare -r XCODE_DIR="$HOME/Library/Developer/Xcode"
+declare -r THEME_DIR="$XCODE_DIR/UserData/FontAndColorThemes"
+declare -r XCODE_ONEMONOKAI_DIR="$PWD/Xcode-OneMonokai"
 
-function msg {
-    printf "%80s\n" ' ' | tr ' ' -
-    printf "%s\n" "$1" 
+function msg() {
+	printf "%80s\n" ' ' | tr ' ' -
+	printf "%s\n" "$1"
 }
 
-function install_theme {
-    msg "> Clonning theme ...  📦"
+function install_theme() {
+	msg "> Clonning theme ...  📦"
 
-    if ! git clone --branch main --depth 1 \
-        "https://github.com/cpea2506/Xcode-OneMonokai.git"; then
-        echo "Failed to clone repository.";
-        exit 1
-    fi
+	if ! git clone --branch main --depth 1 \
+		"https://github.com/cpea2506/Xcode-OneMonokai.git"; then
+		echo "Failed to clone repository."
+		exit 1
+	fi
 }
 
-function setting_theme {
-    declare -r ONEMONOKAI_DIR=$XCODE_ONEMONOKAI_DIR/OneMonokai
+function setting_theme() {
+	declare -r ONEMONOKAI_DIR=$XCODE_ONEMONOKAI_DIR/OneMonokai
 
-    msg "> Setting theme ... 🧩"
+	msg "> Setting theme ... 🧩"
 
-    mkdir -p $THEME_DIR
+	mkdir -p "$THEME_DIR"
 
-    if ! cp $ONEMONOKAI_DIR/OneMonokai.xccolortheme $THEME_DIR; then
-        echo "> Failed to install. Please try again! 🚨"
-        exit 1
-    fi
+	if ! cp "$ONEMONOKAI_DIR/OneMonokai.xccolortheme" "$THEME_DIR"; then
+		echo "> Failed to install. Please try again! 🚨"
+		exit 1
+	fi
 
-    echo "> Setting done! ✅"
+	echo "> Setting done! ✅"
 }
 
-function remove_install_folder {
-    msg "> Remove install folder 💣"
+function remove_install_folder() {
+	msg "> Remove install folder 💣"
 
-    rm -rf $XCODE_ONEMONOKAI_DIR
+	rm -rf "$XCODE_ONEMONOKAI_DIR"
 
-    echo "> Remove successfully ✅"
+	echo "> Remove successfully ✅"
 }
 
-function main {
-    if [ -d $XCODE_DIR ]; then
-        install_theme
-        setting_theme
-        remove_install_folder
-        
-        echo "> You can restart Xcode now. 👌"
-    else
-        echo "Xcode not detected. 🚨"
-    fi
+function main() {
+	if [ -d "$XCODE_DIR" ]; then
+		install_theme
+		setting_theme
+		remove_install_folder
+
+		echo "> You can restart Xcode now. 👌"
+	else
+		echo "Xcode not detected. 🚨"
+	fi
 }
 
 main
-
